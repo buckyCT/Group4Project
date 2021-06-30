721a006ba4d67a3dfe840d1dbd48b81bd1197599
zinaemilian <82812872+zinaemilian@users.noreply.github.com>
2021-06-30 12:22:30 +0100

    work 1

--- src/test/java/ZinaUS009/US009TC01.java
+++ src/test/java/ZinaUS009/US009TC01.java
@@ -3,5 +3,6 @@ package ZinaUS009;
 public class US009TC01 {
     public static void main(String[] args) {
         System.out.println("\"US009TC01 has strated\" = " + "US009TC01 has strated");
+        //test
     }
 }
